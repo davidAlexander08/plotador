6d303ca19ba674bbdf24ff3d9c768f9a3b8ffe53
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-21 19:13:35 -0300

    Update eco_indicadores.py

--- apps/indicadores/eco_indicadores.py
+++ apps/indicadores/eco_indicadores.py
@@ -1,14 +1,9 @@
 from typing import List
 from os.path import join
-from datetime import datetime
-from calendar import monthrange
-import numpy as np
 import pandas as pd
-from inewave.newave import Dger
 from apps.utils.log import Log
 import os.path
 from apps.model.caso import Caso
-from apps.model.usina import UsinaAvalicao
 import warnings
 
 class EcoIndicadores:
@@ -20,29 +15,20 @@ class EcoIndicadores:
 
     DIR_SINTESE = "sintese"
 
-    def __init__(
-        self, casos: List[Caso], nome_caso_referencia: str, usinas: List[UsinaAvalicao]
-    ):
+    def __init__(  self, casos: List[Caso] ):
         warnings.simplefilter(action='ignore')
-        
         self.casos = casos
-        self.usinas = usinas
-        self.nome_caso_referencia = nome_caso_referencia
-
+        
     def retorna_df_concatenado(self,sintese):
         return pd.concat(self.retornaMapaDF(sintese))
     
     def __retorna_df(self, caso, sintese) -> pd.DataFrame:
-        caminho_caso = caso.caminho
-        arq_sintese = join(
-            caminho_caso, self.DIR_SINTESE, sintese+".parquet.gzip"
-        )
+        arq_sintese = join( caso.caminho, self.DIR_SINTESE, sintese+".parquet.gzip"  )
         check_file = os.path.isfile(arq_sintese)
         if(check_file) :
             df = pd.read_parquet(arq_sintese)
             return df
         else:
-            #print("CASO: ", caso.nome, " NAO POSSUI A SINTESE: ", sintese)
             df_vazio = pd.DataFrame()
             return df_vazio
             
