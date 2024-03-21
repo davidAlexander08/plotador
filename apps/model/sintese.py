5509e032a6f1135476c7e5383205473119a2fd55
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-21 20:04:26 -0300

    Update sintese.py

--- apps/model/sintese.py
+++ apps/model/sintese.py
@@ -7,20 +7,22 @@ class Sintese:
     que será utilizado para o cálculo de indicadores
     e para geração de gráficos.
     """
-
+    def retornaFiltro(self, sintese):
+        espacial = sintese.split("_")[1]
+        return self.__mapa_filtro[espacial]
+        
     def __init__(self, sintese: str):
         self.sintese = sintese  
         self.filtro = self.retornaFiltro(sintese)
-
-        self.mapa_filtro = {
+        
+        self.__mapa_filtro = {
             "SBM": "submercado",
             "REE": "ree",
             "UHE": "usina",
             "SIN": None
         }
-    def retornaFiltro(self, sintese):
-        espacial = sintese.split("_")[1]
-        return self.mapa_filtro[espacial]
+        
+
 
     @classmethod
     def from_dict(cls, d: Dict[str, str]):
