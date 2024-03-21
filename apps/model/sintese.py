03a6c2ee53ef95f74953768cdf920a17efda42f1
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-21 19:56:04 -0300

    Update sintese.py

--- apps/model/sintese.py
+++ apps/model/sintese.py
@@ -10,6 +10,17 @@ class Sintese:
 
     def __init__(self, sintese: str):
         self.sintese = sintese  
+        self.filtro = self.retornaFiltro(sintese)
+
+        self.mapa_filtro = {
+            "SBM": "submercado",
+            "REE": "ree",
+            "UHE": "usina"
+            "SIN": None
+        }
+    def retornaFiltro(self, sintese):
+        espacial = sintese.split("_")[1]
+        return self.mapa_filtro[espacial]
 
     @classmethod
     def from_dict(cls, d: Dict[str, str]):
