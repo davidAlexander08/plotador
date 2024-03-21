0be8aa89eee59c0bd3fc5cca20d29af8b3740041
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-21 19:40:00 -0300

    Update unidade.py

--- apps/model/unidade.py
+++ apps/model/unidade.py
@@ -1,10 +1,20 @@
 
 class UnidadeSintese:    
-    def __init__(self, sintese, legendaEixoY, legendaEixoX, titulo, fitroColuna = None, filtroArgumento = None, df = None):
+    def __init__(self, sintese, legendaEixoX, titulo, fitroColuna = None, filtroArgumento = None, df = None):
         self.sintese = sintese
-        self.legendaEixoY = legendaEixoY     
+        self.legendaEixoY = self.label_y(sintese)     
         self.legendaEixoX = legendaEixoX
         self.titulo = titulo
         self.df = df
         self.fitroColuna = fitroColuna
         self.filtroArgumento = filtroArgumento
+
+    def label_y(self, sintese):
+        operacional = sintese.split("_")[0]
+        return mapa_sintese_label_y[operacional]
+mapa_sintese_label_y ={
+    "GTER":"MWmes",
+    "GHID":"MWmes",
+    "QTUR":"m3/s",
+    "QDEF":"m3/s"
+}
