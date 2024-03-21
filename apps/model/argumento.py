c9e73af678827bcbae3ccd65e46b9e36a453f3bc
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-21 20:59:11 -0300

    Update argumento.py

--- apps/model/argumento.py
+++ apps/model/argumento.py
@@ -15,5 +15,5 @@ class Argumento:
     @classmethod
     def from_dict(cls, d: Dict[str, str]):
         key = list(d.keys())[0]
-        print(key)
+        print(str(key))
         return cls(d[key]["nome"], key)
