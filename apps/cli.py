856d6e56bd78812971da03e8fdc25123d7fa23e1
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-21 20:19:48 -0300

    Update cli.py

--- apps/cli.py
+++ apps/cli.py
@@ -389,32 +389,29 @@ def analise_temporal(arquivo_json):
 
         
         
-        unity = UnidadeSintese("EARPF_SIN_EST", None, "%", "Energia_Armazenada_Percentual_Final_SIN_CREF "+estudo)
-        df_unity = indicadores_temporais.retorna_df_concatenado(unity.sintese, unity.fitroColuna , unity.filtroArgumento )
-        graficos.gera_graficos_linha_Newave_CREF(df_unity, indicadores_temporais.df_cref, "EARPF", unity.legendaEixoY, unity.titulo, None).write_image(
-            os.path.join(diretorio_saida, "SIN_EARPF_CREF"+estudo+".png"),
-            width=800,
-            height=600
-            )
-        graficos.gera_graficos_linha_Newave_CREF(df_unity, indicadores_temporais.df_cref, "EARPF", unity.legendaEixoY, unity.titulo+"_2024", "2024").write_image(
-            os.path.join(diretorio_saida, "SIN_EARPF_CREF_2024"+estudo+".png"),
-            width=800,
-            height=600
-            )
-        graficos.gera_graficos_linha_Newave_CREF(df_unity, indicadores_temporais.df_cref, "EARPF", unity.legendaEixoY, unity.titulo+"_Ano_Vigente", "ADEQUA").write_image(
-            os.path.join(diretorio_saida, "SIN_EARPF_CREF_AnoCaso"+estudo+".png"),
-            width=800,
-            height=600
-            )
-        
-
-
-        df_EARPF_mean_p10_p90 = indicadores_temporais.gera_df_mean_p10_p90("EARPF_SIN_EST")
-        graficos.gera_graficos_linha_mean_p10_p90_CREF(df_EARPF_mean_p10_p90,indicadores_temporais.df_cref, "EARPF", "%", "Energia Armazenada Cenarios CREF"+estudo, None ).write_image(
-            os.path.join(diretorio_saida, "Energia_Armazenada_Media_P10_P90_"+estudo+".png"),
-            width=800,
-            height=600
-            )
+        #unity = UnidadeSintese("EARPF_SIN_EST", None, "%", "Energia_Armazenada_Percentual_Final_SIN_CREF "+estudo)
+        #df_unity = indicadores_temporais.retorna_df_concatenado(unity.sintese, unity.fitroColuna , unity.filtroArgumento )
+        #graficos.gera_graficos_linha_Newave_CREF(df_unity, indicadores_temporais.df_cref, "EARPF", unity.legendaEixoY, unity.titulo, None).write_image(
+        #    os.path.join(diretorio_saida, "SIN_EARPF_CREF"+estudo+".png"),
+        #    width=800,
+        #    height=600
+        #    )
+        #graficos.gera_graficos_linha_Newave_CREF(df_unity, indicadores_temporais.df_cref, "EARPF", unity.legendaEixoY, unity.titulo+"_2024", "2024").write_image(
+        #    os.path.join(diretorio_saida, "SIN_EARPF_CREF_2024"+estudo+".png"),
+        #    width=800,
+        #    height=600
+        #    )
+        #graficos.gera_graficos_linha_Newave_CREF(df_unity, indicadores_temporais.df_cref, "EARPF", unity.legendaEixoY, unity.titulo+"_Ano_Vigente", "ADEQUA").write_image(
+        #    os.path.join(diretorio_saida, "SIN_EARPF_CREF_AnoCaso"+estudo+".png"),
+        #    width=800,
+        #    height=600
+        #    )
+        #df_EARPF_mean_p10_p90 = indicadores_temporais.gera_df_mean_p10_p90("EARPF_SIN_EST")
+        #graficos.gera_graficos_linha_mean_p10_p90_CREF(df_EARPF_mean_p10_p90,indicadores_temporais.df_cref, "EARPF", "%", "Energia Armazenada Cenarios CREF"+estudo, None ).write_image(
+        #    os.path.join(diretorio_saida, "Energia_Armazenada_Media_P10_P90_"+estudo+".png"),
+        #    width=800,
+        #    height=600
+        #    )
 
 
     else:
