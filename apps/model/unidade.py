e323c05556b160aa029d44294e565e5b986c25ed
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-21 19:49:10 -0300

    Update unidade.py

--- apps/model/unidade.py
+++ apps/model/unidade.py
@@ -1,10 +1,19 @@
 
-class UnidadeSintese:    
+class UnidadeSintese:  
+    def label_y(self, sintese):
+        operacional = sintese.split("_")[0]
+        return self.__mapa_sintese_label_y[operacional]
+
+    def titulo_fig(self, sintese):
+        operacional = sintese.split("_")[0]
+        espacial = sintese.split("_")[1]
+        return self.__mapa_sintese_titulo[operacional]+" "+espacial
+        
     def __init__(self, sintese, legendaEixoX, titulo, fitroColuna = None, filtroArgumento = None, df = None):
         self.sintese = sintese
         self.legendaEixoY = self.label_y(sintese)     
         self.legendaEixoX = legendaEixoX
-        self.titulo = titulo
+        self.titulo = self.titulo_fig(sintese)
         self.df = df
         self.fitroColuna = fitroColuna
         self.filtroArgumento = filtroArgumento
@@ -19,9 +28,19 @@ class UnidadeSintese:
             "QDEF":"m3/s",
             "VVMINOP":"MWmed",
         }
-    def label_y(self, sintese):
-        operacional = sintese.split("_")[0]
-        return self.__mapa_sintese_label_y[operacional]
+
+        self.__mapa_sintese_titulo =        {
+            "GTER":"Geração Térmica",
+            "GHID":"Geração Hidrelétrica",
+            "EARPF":"Energ. Armz. Perc.",
+            "COP":"Custo de Oper.",
+            "EARMF":"Energ. Armz.",
+            "CMO":"Custo Marg.",
+            "QTUR":"Vaz. Turb.",
+            "QDEF":"Vaz. Defl.",
+            "VVMINOP":"Viol. VMINOP",
+        }
+
 
 
 
