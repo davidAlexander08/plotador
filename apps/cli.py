9d9ed8f130e7412d97bb80cd8cbcf59df083e569
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-21 21:42:32 -0300

    Update cli.py

--- apps/cli.py
+++ apps/cli.py
@@ -340,20 +340,16 @@ def analise_temporal(arquivo_json):
             for arg in args:
                 if(espacial == arg.chave):
                     unity = UnidadeSintese(sts.sintese, "estagios", sts.filtro, arg.nome)
-                #for unity in listaUnidadesGraficas:
+                    diretorio_saida = diretorio_saida+"/"+arg.nome
+                    os.makedirs(diretorio_saida, exist_ok=True)
+                    
                     df_unity = indicadores_temporais.retorna_df_concatenado(unity.sintese, unity.fitroColuna , unity.filtroArgumento )
                     indicadores_temporais.exportar(df_unity, diretorio_saida,  "eco_"+unity.titulo+"_"+estudo)
-                    #Log.log().info("Gerando tabela "+unity.titulo)
-                    #df_unity.to_csv( os.path.join(diretorio_saida, "eco_"+unity.titulo+"_"+estudo+".csv"), index=False )
-                    
                     fig = graficos.gera_grafico_linha(df_unity, unity.legendaEixoY , unity.legendaEixoX, unity.titulo+"_"+estudo)
                     graficos.exportar(fig, diretorio_saida, "eco_"+unity.titulo+"_"+estudo)
                     
                     df_unity_2_mes = indicadores_temporais.retorna_df_concatenado_medio_2_mes(unity.sintese, unity.fitroColuna , unity.filtroArgumento )
                     indicadores_temporais.exportar(df_unity_2_mes, diretorio_saida,  "eco_"+unity.titulo+"_2_mes_"+estudo)
-                    #Log.log().info("Gerando tabela 2 mes "+unity.titulo)
-                    #df_unity_2_mes.to_csv( os.path.join(diretorio_saida, "eco_"+unity.titulo+"_2_mes_"+estudo+".csv"), index=False )
-                    
                     fig = graficos.gera_grafico_barra(df_unity_2_mes["valor"], df_unity_2_mes["caso"],  unity.legendaEixoX, unity.legendaEixoY, 2, unity.titulo+"2_mes")
                     graficos.exportar(fig, diretorio_saida, unity.titulo+"_2_mes_"+estudo)
 
