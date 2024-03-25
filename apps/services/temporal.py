93e62bfec29be8262f2a318839df4abb957983ad
davidAlexander08 <david.tbsilva@gmail.com>
2024-03-25 08:58:34 -0300

    services

---
+++ apps/services/temporal.py
@@ -0,0 +1,28 @@
+from typing import Dict
+
+
+class Sintese:
+    """
+    Classe base que representa uma sintese do estudo
+    que será utilizado para o cálculo de indicadores
+    e para geração de gráficos.
+    """
+    def retornaFiltro(self, sts):
+        espacial = sts.split("_")[1]
+        return self.__mapa_filtro[espacial]
+        
+    def __init__(self, sts: str):
+        self.__mapa_filtro = {
+            "SBM": "submercado",
+            "REE": "ree",
+            "UHE": "usina",
+            "BCA": "bacia",
+            "SIN": None
+        }
+        self.sintese = sts  
+        self.filtro = self.retornaFiltro(sts)
+
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, str]):
+        return cls(d["sintese"])
