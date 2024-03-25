a9e314bfaa8059575357be38fdf538be1576a1ea
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-25 09:41:58 -0300

    Update media.py

--- apps/services/media.py
+++ apps/services/media.py
@@ -24,9 +24,9 @@ class Media:
           sinteses = [Sintese.from_dict(d) for d in dados["sinteses"]]
           args = [Argumento.from_dict(d) for d in dados["argumentos"]]
           
-          indicadores_medios = IndicadoresMedios(casos, self.nome_caso_referencia)
-          indicadores_temporais = IndicadoresTemporais(casos)
-          graficos = Graficos(casos)
+          self.indicadores_medios = IndicadoresMedios(casos, self.nome_caso_referencia)
+          self.indicadores_temporais = IndicadoresTemporais(casos)
+          self.graficos = Graficos(casos)
           # Gera saídas do estudo
           diretorio_saida = f"resultados/{self.estudo}/media"
           os.makedirs(diretorio_saida, exist_ok=True)
@@ -50,14 +50,14 @@ class Media:
                         
 
     def executa(self, unity, diretorio_saida_arg):
-        df_unity = indicadores_medios.retorna_df_concatenado(unity)
-        indicadores_medios.exportar(df_unity, diretorio_saida_arg,  "medias_"+unity.titulo+"_"+self.estudo)
+        df_unity = self.indicadores_medios.retorna_df_concatenado(unity)
+        self.indicadores_medios.exportar(df_unity, diretorio_saida_arg,  "medias_"+unity.titulo+"_"+self.estudo)
 
-        fig = graficos.gera_grafico_barra(df_unity["valor"], df_unity["caso"],  unity.legendaEixoX, unity.legendaEixoY, 2, unity.titulo)
-        graficos.exportar(fig, diretorio_saida_arg, "medias_"+unity.titulo+"_"+self.estudo)
+        fig = self.graficos.gera_grafico_barra(df_unity["valor"], df_unity["caso"],  unity.legendaEixoX, unity.legendaEixoY, 2, unity.titulo)
+        self.graficos.exportar(fig, diretorio_saida_arg, "medias_"+unity.titulo+"_"+self.estudo)
 
-        df_unity_incr = indicadores_medios.retorna_DF_cenario_medio_incremental_percentual(unity)
-        indicadores_medios.exportar(df_unity_incr, diretorio_saida_arg, "medias_incr_"+unity.titulo+"_"+self.estudo)
+        df_unity_incr = self.indicadores_medios.retorna_DF_cenario_medio_incremental_percentual(unity)
+        self.indicadores_medios.exportar(df_unity_incr, diretorio_saida_arg, "medias_incr_"+unity.titulo+"_"+self.estudo)
         
-        fig = graficos.gera_grafico_barra(df_unity_incr["valor"], df_unity_incr["caso"],  unity.legendaEixoX, unity.legendaEixoY, 2, unity.titulo)
-        graficos.exportar(fig, diretorio_saida_arg, "medias_incr_"+unity.titulo+"_"+self.estudo)
+        fig = self.graficos.gera_grafico_barra(df_unity_incr["valor"], df_unity_incr["caso"],  unity.legendaEixoX, unity.legendaEixoY, 2, unity.titulo)
+        self.graficos.exportar(fig, diretorio_saida_arg, "medias_incr_"+unity.titulo+"_"+self.estudo)
