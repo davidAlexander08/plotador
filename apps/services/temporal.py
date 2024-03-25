18a547f39280f5441acc7427f38da3b74cb7c818
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-25 09:33:02 -0300

    Update temporal.py

--- apps/services/temporal.py
+++ apps/services/temporal.py
@@ -40,7 +40,7 @@ class Temporal:
                     self.executa(unity,diretorio_saida_arg )
                 else:
                     for arg in args:
-                        if(espacial == arg.chave)):
+                        if(espacial == arg.chave):
                             diretorio_saida_arg = diretorio_saida+"/"+arg.chave+"/"+arg.nome
                             os.makedirs(diretorio_saida_arg, exist_ok=True)
                             unity = UnidadeSintese(sts, "estagios", arg)
