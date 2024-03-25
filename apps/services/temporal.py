4da198922a0b3627357774aad75a2a44eb368d24
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-25 09:19:47 -0300

    Update temporal.py

--- apps/services/temporal.py
+++ apps/services/temporal.py
@@ -35,9 +35,9 @@ class Temporal:
                 if(espacial == "SIN"):
                     arg = Argumento(None, None)
                     unity = UnidadeSintese(sts, "estagios", arg)
-                        diretorio_saida_arg = diretorio_saida+"/SIN
-                        os.makedirs(diretorio_saida_arg, exist_ok=True)
-                        self.executa(unity,diretorio_saida_arg )
+                    diretorio_saida_arg = diretorio_saida+"/SIN
+                    os.makedirs(diretorio_saida_arg, exist_ok=True)
+                    self.executa(unity,diretorio_saida_arg )
                     
                 for arg in args:
                     if((espacial == arg.chave)):
