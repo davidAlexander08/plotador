7d895eae2f089e64b4b66d92488617d08efba306
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-26 14:26:46 -0300

    Update indicadores_cenarios.py

--- apps/indicadores/indicadores_cenarios.py
+++ apps/indicadores/indicadores_cenarios.py
@@ -8,37 +8,28 @@ from inewave.newave import Dger
 from apps.utils.log import Log
 import os.path
 from apps.model.caso import Caso
-from apps.model.usina import UsinaAvalicao
-from apps.indicadores.indicadores_temporais import IndicadoresTemporais
 from apps.indicadores.eco_indicadores import EcoIndicadores
 import warnings
 
-class IndicadoresCenarios:
+class IndicadoresCenarios(EcoIndicadores):
 
     DIR_SINTESE = "sintese"
 
-    def __init__(
-        self, casos: List[Caso], nome_caso_referencia: str, usinas: List[UsinaAvalicao]
-    ):
+    def __init__(self, casos: List[Caso] ):
         warnings.simplefilter(action='ignore')
-        
         self.casos = casos
-        self.usinas = usinas
-        self.nome_caso_referencia = nome_caso_referencia
-      
-        self.indicadores_temporais = IndicadoresTemporais(casos, nome_caso_referencia,usinas)
-        self.eco_indicadores = EcoIndicadores(casos, nome_caso_referencia,usinas)
+        EcoIndicadores.__init__(self, casos)
 
 
-    def mapa_df_cenario(self, sintese, coluna, argumento_filtro):
-      eco_mapa  = self.eco_indicadores.retornaMapaDF(sintese)
+    def mapa_df_cenario(self, unidade):
+      eco_mapa  = self.retornaMapaDF(unidade)
       mapa_temporal = {}
-      if( (coluna is None) & (argumento_filtro is None) ):
+      if( (unidade.fitroColuna is None) & (unidade.filtroArgumento is None) ):
           return eco_mapa
       else:
-          for c in self.casos: eco_mapa[c] = eco_mapa[c].loc[eco_mapa[c][coluna] == argumento_filtro]
+          for c in self.casos: eco_mapa[c] = eco_mapa[c].loc[eco_mapa[c][unidade.fitroColuna] == unidade.filtroArgumento]
           mapa_temporal = eco_mapa
       return mapa_temporal
 
-    def retorna_df_concatenado(self,sintese, coluna = None, argumento_filtro = None):
-        return pd.concat(self.mapa_df_cenario(sintese, coluna, argumento_filtro))
+    def retorna_df_concatenado(self,unidade):
+        return pd.concat(self.mapa_df_cenario(unidade))
