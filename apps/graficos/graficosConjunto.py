bb6c81e6ac36acd188f6e25553a53978881152e4
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-27 20:20:17 -0300

    debug

--- apps/graficos/graficosConjunto.py
+++ apps/graficos/graficosConjunto.py
@@ -106,8 +106,8 @@ class GraficosConjunto:
                 fig_subplot.layout.annotations[contador_titulo].update(text=caso.nome)
                 contador_titulo += 1
 
-        fig_subplot.update_layout(title = dict(text = titulo, title_font_family="Times New Roman",font=dict(size= unidade.tamanho_texto)),
-                                  legend=dict(title_font_family="Times New Roman", font=dict(size= unidade.tamanho_texto))
+        fig_subplot.update_layout(title = dict(text = titulo,font=dict(size= unidade.tamanho_texto)),
+                                  legend=dict(font=dict(size= unidade.tamanho_texto))
                                  )
         mapaFiguras["subplot"+titulo] = fig_subplot
         return mapaFiguras
