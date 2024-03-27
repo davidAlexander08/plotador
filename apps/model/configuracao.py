2cc5a60af73b9d67d3149b9a7cc2c8a54cd87ebc
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-26 21:12:20 -0300

    create configuracao

---
+++ apps/model/configuracao.py
@@ -0,0 +1,10 @@
+from typing import Dict
+
+class Configuracao:
+    def __init__(self, sintese: str, argumento: str):
+        self.sintese = sintese
+        self.argumento = argumento
+
+    @classmethod
+    def from_dict(cls, d: Dict[str, str]):
+        return cls(d["sintese"], d["argumento"])
--- apps/model/dados_json_caso.py
+++ apps/model/dados_json_caso.py
@@ -7,14 +7,7 @@ import os
 import json
 from typing import Dict
 
-class Configuracao:
-    def __init__(self, sintese: str, argumento: str):
-        self.sintese = sintese
-        self.argumento = argumento
 
-    @classmethod
-    def from_dict(cls, d: Dict[str, str]):
-        return cls(d["sintese"], d["argumento"])
 
 
 class Dados_json_caso(MetaData):
