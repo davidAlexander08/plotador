98f3941c6dfbb8d87fa2f917313c8be3e0c62989
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-04-02 18:59:05 -0300

    objeto figura para plotar

--- apps/graficos/figura.py
+++ apps/graficos/figura.py
@@ -7,8 +7,19 @@ import plotly.graph_objects as go
 
 class Figura():
 
-    def __init__(self, col = 1, row = 1):
-        self.fig = make_subplots(rows=row, cols=col, subplot_titles=(" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "))
-    
-    def add_trace(self, goObject, coluna = 1, linha = 1):
-        self.fig.add_trace(goObject, row = linha , col = coluna)
+    def __init__(self, conjUnity , mapaGO, titulo):
+        self.titulo = titulo
+        self.fig = make_subplots(rows=conjUnity.arg.max_lin, cols=conjUnity.arg.max_col, subplot_titles=(" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "))
+        for unity in conjUnity.listaUnidades:
+            self.fig.add_trace(mapaGO[unity], row = unity.arg.lin, col = unity.arg.col)
+            self.fig.update_xaxes(title=conjUnity.legendaEixoX, row = unity.arg.lin , col = unity.arg.col) 
+            self.fig.update_yaxes(title=conjUnity.legendaEixoY, row = unity.arg.lin , col = unity.arg.col) 
+            if(len(conjUnity.listaUnidades) > 1):
+                self.fig.layout.annotations[unity.arg.t].update(text=unity.arg.nome) 
+            self.fig.update_layout(title= titulo)
+            self.fig.update_layout(font=dict(size= unidade.tamanho_texto), 
+                                yaxis=dict(range=[limInf,limSup])
+                            ) 
+
+    def getFig(self):
+        return self.fig
