d8884ec8be3347680912f6deb75ab3d1a000287d
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-06-25 16:16:19 -0300

    teste_fcf_decomp_ativos_e_totais

--- apps/services/fcf.py
+++ apps/services/fcf.py
@@ -52,7 +52,8 @@ class FCF:
                         lista_df_casos = []
                         for caso in self.casos:
                             df = self.cortes_ativos_decomp(unity, caso)
-                            lista_df_casos.append(df)
+                            lista_df_casos.append(df[0])
+
                         df_cortes_ativos_todos_casos = pd.concat(lista_df_casos)
                         df_cortes_ativos_todos_casos.to_csv("pis_ativos"+unity.arg.nome+self.estudo+".csv")
 
@@ -67,7 +68,7 @@ class FCF:
                         fig.update_xaxes(title_text="Casos")
                         fig.update_yaxes(title_text="1000R$/hm3")
                         fig.update_yaxes(range=[-1400,0])
-                        fig.update_layout(font=dict(size= 15))
+                        fig.update_layout(font=dict(size= 25))
                         fig.write_image(
                             os.path.join(diretorio_saida+"pis_ativos"+unity.arg.nome+self.estudo+".png"),
                             width=800,
@@ -96,7 +97,7 @@ class FCF:
                         )
                         fig.update_xaxes(title_text="Casos")
                         fig.update_yaxes(title_text="1000$/hm3")
-                        fig.update_layout(font=dict(size= 13))
+                        fig.update_layout(font=dict(size= 25))
                         fig.update_layout(title=" PIvs Ativos "+unity.arg.nome)
                         fig.write_image(
                             os.path.join(diretorio_saida+"/pivs_ativos_"+unity.arg.nome+self.estudo+".png"),
@@ -149,10 +150,11 @@ class FCF:
         df_fcf = pd.DataFrame()
         if(os.path.isfile(arq_fcfnwi)):
             fcf = Fcfnw.read(arq_fcfnwi)
-            df = fcf.cortes
+            df = fcf.cortes.copy()
             df_fcf = df.loc[(df["UHE"] == codigo)].reset_index(drop = True)
-        
-        
+            df_fcf["coef"] = df_fcf["coef_varm"]
+            df_fcf = df_fcf.drop(["coef_varm"], axis = 1)
+
         arq_fcfnwn = caso.caminho+"/fcfnwn."+extensao
         arq_dadger = caso.caminho+"/dadger."+extensao
         f_prodt_65 = 0
@@ -162,9 +164,9 @@ class FCF:
             dadger_uh = dadger.uh(df = True)
             codigo_ree = dadger_uh.loc[dadger_uh["codigo_usina"] == codigo]["codigo_ree"].iloc[0]
             fcf = Fcfnw.read(arq_fcfnwn)
-            df = fcf.cortes
+            df = fcf.cortes.copy()
             df_fcf = df.loc[(df["REE"] == codigo_ree)].reset_index(drop = True)
-
+            df_fcf["caso"] = caso.nome
             arq_memcal = caso.caminho+"/memcal."+extensao
             if(os.path.isfile(arq_memcal)):
                 pass
@@ -178,6 +180,10 @@ class FCF:
                         #print(line[25:50])
                         f_prodt_65 = float(line[25:50].strip())
                         flag = 0
+            df_fcf["coef_earm"] = ((df_fcf["coef_earm"]*f_prodt_65*10000)/36)/1000 # PARANAUE
+            df_fcf["coef"] = df_fcf["coef_earm"]
+            df_fcf = df_fcf.drop(["coef_earm"], axis = 1)
+
             else:
                 raise FileNotFoundError(f"Arquivo memcal.rvx não encontrado.") 
 
@@ -188,10 +194,7 @@ class FCF:
             #print(cortes_ativos)
             valor_coef = 0
             for corte in lista_cortes_ativos:
-                if(not os.path.isfile(arq_fcfnwi)):
-                    coef_fcf_corte = ((df_fcf.loc[(df_fcf["corte"] == corte)]["coef_earm"].iloc[0]*f_prodt_65*10000)/36)/1000 ## PARANAUE
-                if(os.path.isfile(arq_fcfnwi)):
-                    coef_fcf_corte = df_fcf.loc[(df_fcf["corte"] == corte)]["coef_varm"].iloc[0]
+                    coef_fcf_corte = df_fcf.loc[(df_fcf["corte"] == corte)]["coef"].iloc[0]
                 parcela_pi = cortes_ativos.loc[(cortes_ativos["indice_corte"]==corte)]["parcela_pi"].iloc[0]
                 valor_coef += coef_fcf_corte*parcela_pi
             valor_coef = valor_coef/coef_pi
@@ -199,7 +202,10 @@ class FCF:
             lista_df_cortes_ativos_ponderados.append(df_atv)
         df_cortes_ativos_ponderados = pd.concat(lista_df_cortes_ativos_ponderados).reset_index(drop=True)
         df_cortes_ativos_ponderados["caso"] = caso.nome
-        return df_cortes_ativos_ponderados
+
+
+
+        return (df_cortes_ativos_ponderados, df_fcf)
 
 
 
